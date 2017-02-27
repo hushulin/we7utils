bb564343b8ff5ce670c183a3f52e9c1a4174f607
hushulin <hushulin12321@126.com>
2017-02-28 00:20:11 +0800

    0

--- test.php
+++ test.php
@@ -3,13 +3,36 @@ require __DIR__ . '/vendor/autoload.php';
 
 $settings = [
 	'settings' => [
-		'host' => '127.0.0.1',
+		// Database settings
+		'db' => [
+		// 'write' => [
+		// 'host' => ['192.168.1.5','192.168.1.5','192.168.1.5'],
+		// ],
+		// 'read' => [
+		// 'host' => [
+		// '192.168.1.5',
+		// '192.168.1.5',
+		// '192.168.1.5',
+		// '192.168.1.5',
+		// '192.168.1.5',
+		// '192.168.1.5',
+		// ],
+		// ],
+		'driver' => 'mysql',
+		'host' => '114.115.212.17',
+		'database' => 'we7',
+		'username' => 'root',
+		'password' => 'hushulin', //
+		'charset' => 'utf8',
+		'collation' => 'utf8_general_ci',
+		'prefix' => 'ims_',
+		],
 	],
 ];
 
 $app = new Bow\App($settings);
 $container = $app->getContainer();
 
-$cache = $container->get('settings');
+$cache = $container->get('db');
 
-var_dump($cache);
+var_dump($cache->table('users')->get());
