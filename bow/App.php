ad033abc48e1785f4068758e2c452c091b298573
hushulin <hushulin12321@126.com>
2017-02-27 23:48:14 +0800

    0

--- bow/App.php
+++ bow/App.php
@@ -1,11 +1,68 @@
 <?php
 namespace Bow;
-class App {
-	
-	private $container;
 
-	function getContainer()
-	{
-		return $this->container;
-	}
+use Slim\Container;
+use InvalidArgumentException;
+use Interop\Container\ContainerInterface;
+
+class App
+{
+
+    /**
+     * Container
+     *
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /********************************************************************************
+     * Constructor
+     *******************************************************************************/
+
+    /**
+     * Create new application
+     *
+     * @param ContainerInterface|array $container Either a ContainerInterface or an associative array of app settings
+     * @throws InvalidArgumentException when no container is provided that implements ContainerInterface
+     */
+    public function __construct($container = [])
+    {
+        if (is_array($container)) {
+            $container = new Container($container);
+        }
+        if (!$container instanceof ContainerInterface) {
+            throw new InvalidArgumentException('Expected a ContainerInterface');
+        }
+        $this->container = $container;
+    }
+
+    /**
+     * Enable access to the DI container by consumers of $app
+     *
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+     * Calling a non-existant method on App checks to see if there's an item
+     * in the container that is callable and if so, calls it.
+     *
+     * @param  string $method
+     * @param  array $args
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        if ($this->container->has($method)) {
+            $obj = $this->container->get($method);
+            if (is_callable($obj)) {
+                return call_user_func_array($obj, $args);
+            }
+        }
+
+        throw new \BadMethodCallException("Method $method is not a valid method");
+    }
 }
--- test.php
+++ test.php
@@ -1,7 +1,15 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+$settings = [
+	'settings' => [
+		'host' => '127.0.0.1',
+	],
+];
+
 $app = new Bow\App($settings);
 $container = $app->getContainer();
 
-$cache = $container->get('cache');
+$cache = $container->get('settings');
+
+var_dump($cache);
